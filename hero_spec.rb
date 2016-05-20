fa6985494bb8adc379733015215daceb64f6d687
nogaasha <sivosha2010@gmail.com>
2016-05-20 20:41:07 +0300

    Add tests

--- hero_spec.rb
+++ hero_spec.rb
@@ -2,4 +2,14 @@ require './hero/'
 
 describe Hero do
   
+  it "has a capitalized name"  do
+     hero = Hero.new 'mike'
+     expect(hero.name).to eq 'Mike'
+  end  
+
+  it "can power up" do 
+    hero = Hero.new 'mike'
+    expect(hero.power_up).to eq 110
+   end
+
 end
