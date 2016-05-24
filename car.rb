c9bf3470a91d7713dc4f1531bd8aeb185edeafe2
nogaasha <sivosha2010@gmail.com>
2016-05-24 23:09:37 +0300

    close bag #1

--- car.rb
+++ car.rb
@@ -12,10 +12,10 @@ class Car
   
   #как далеко мы можем уехать
   def range
-    0
+    @fuel * MILES_PER_GALLON
   end  
 end
 
-car = Car.new
-car.add_fuel 10
-puts "Range is #{car.range} miles"
+ # car = Car.new
+ # car.add_fuel 10
+ # puts "Range is #{car.range} miles"
